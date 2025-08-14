fb852c368288f19b9e4e2df8534d42256f0bbf35
seannemeth <sean.nemeth@broadfording.com>
2025-08-13 21:58:13 -0400

    Delete dic-fixed-server directory

--- dic-fixed-server/src/lib/elo.ts
+++
@@ -1,1 +0,0 @@
-export function expected(a:number,b:number){return 1/(1+Math.pow(10,(b-a)/400))}; export function updateElo(a:number,b:number,scoreA:0|0.5|1,k=20){const ea=expected(a,b),eb=expected(b,a); return [a + k*(scoreA-ea), b + k*((1-scoreA)-eb)];}
