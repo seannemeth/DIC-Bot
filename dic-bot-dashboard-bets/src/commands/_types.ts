708137a586f9d0db4b45df58ee28ff376038aaab
seannemeth <sean.nemeth@broadfording.com>
2025-08-13 21:36:05 -0400

    Delete dic-bot-dashboard-bets directory

--- dic-bot-dashboard-bets/src/commands/_types.ts
+++
@@ -1,7 +0,0 @@
-import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
-
-export type SlashCommand = {
-  data: SlashCommandBuilder;
-  execute: (interaction: ChatInputCommandInteraction) => Promise<void>;
-  adminOnly?: boolean;
-};
